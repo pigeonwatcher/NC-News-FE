9cfd134df3a41a9589342bba41ddda9916132d1c
Benjamin Curry <benjamincurry777@gmail.com>
2024-02-06 13:42:24 +0000

    7-vote-article

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,9 +1,10 @@
 import Comment from './Comment';
+import { getComments } from '../api';
 import { useState, useEffect } from 'react';
 
 export default function CommentList({ article_id }) {
 
-    const { comments, isLoading, error } = fetchComments(article_id);
+    const { comments, isLoading, error } = getComments(article_id);
 
     if (isLoading) return <div className='comment-list'>Loading...</div>;
     if (error) return <div className='comment-list'>Error: {error.msg}</div>;
@@ -17,32 +18,3 @@ export default function CommentList({ article_id }) {
         </ul>
     )
 }
-
-const fetchComments = (article_id) => {
-    const [comments, setComments] = useState();
-    const [isLoading, setLoading] = useState(true); 
-    const [error, setError] = useState(); 
-
-    useEffect(() => {
-        const fetchComments = async () => {
-            try {
-                setLoading(true);
-                const response = await fetch(`https://nc-news-th0a.onrender.com/api/articles/${article_id}/comments`);
-                if (!response.ok) { 
-                    return Promise.reject();
-                }
-                const { comments } = await response.json();
-                setComments(comments);
-            } catch (err) {
-                setError(err);
-            } finally { 
-                setLoading(false);
-            }
-        };
-
-        fetchComments();
-
-    }, [article_id]);
-
-    return { comments, isLoading, error };
-};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import ArticleList from '../components/ArticleList';
+import { getArticles } from '../api';
 import { useState, useEffect } from 'react';
 
 export default function HomePage() {
 
-    const { articles, isLoading, error } = fetchArticles();
+    const { articles, isLoading, error } = getArticles();
 
     if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.msg}</div>;
+    if (error) {console.log(error); return <div>Error: {error.msg}</div>;}
     if (articles.length === 0 || articles === undefined) return <div>No articles found</div>;
 
     return (
@@ -14,31 +15,4 @@ export default function HomePage() {
         <ArticleList articles={articles} />
         </>
     )
-}
-
-function fetchArticles() {
-    const [articles, setArticles] = useState([]);
-    const [isLoading, setLoading] = useState(true);
-    const [error, setError] = useState();
-
-    useEffect(() => {
-        const getArticles = async () => {
-            try {
-                const response = await fetch('https://nc-news-th0a.onrender.com/api/articles');
-                if (!response.ok) {
-                    return Promise.reject();
-                }
-                const { articles:allArticles } = await response.json();
-                setArticles(allArticles);
-            } catch(err) {
-                setError(err)
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        getArticles();
-    }, [])
-
-    return { articles, isLoading, error }
 }
